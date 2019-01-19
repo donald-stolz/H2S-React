57dbae4b1186d5ec81faba8aaaddd6345a2b6f79
donald-stolz <donaldtstolz@gmail.com>
2019-01-19 13:42:32 -0800

    Added filler elements

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return <div className={styles.header} />;
+};
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Home.module.css";
+
+const Header = () => {
+  return <div className={styles.home} />;
+};
+
+export default Header;
